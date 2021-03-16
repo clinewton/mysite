f5c6dcf9fa81b9e509a2fe73c6d6f4726b3711e7
clinewton <clinton.newton@strathmore.edu>
2021-03-16 12:05:16 +0000

    Demo

--- gceo/urls.py
+++ gceo/urls.py
@@ -1,6 +1,7 @@
 from django.urls import include,path
-from gceo.views import HomeView
+from gceo.views import HomeView, DemoView
 
 urlpatterns = [
-    path("", HomeView.as_view(), name="home")
+    path("", HomeView.as_view(), name="home"),
+    path("demo", DemoView.as_view(), name="demo")
 ]
--- gceo/views.py
+++ gceo/views.py
@@ -9,3 +9,10 @@ class HomeView(View):
 
     def get(self, request):
         return render(request, self.template_name,{})
+
+class DemoView(View):
+    # model = Home
+    template_name = "gceo/demo.html"
+
+    def get(self, request):
+        return render(request, self.template_name,{})
